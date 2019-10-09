b9064684d4b47316a0cdb94bcfb5f46bd33999dc
lizi1203 <1095036488@qq.com>
2019-10-09 20:37:42 +0800

    test2

--- app/src/main/java/com/example/myapplication/FirstActivity.java
+++ app/src/main/java/com/example/myapplication/FirstActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -41,7 +42,8 @@ public class FirstActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(FirstActivity.this, "You clicked button1", Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(FirstActivity.this,SecondActivity.class);
+                startActivity(intent);
             }
         });
     }
